d5457d59724c96fbafdb016a9294f3657c66a128
TBEA <74006945+RealTBEA@users.noreply.github.com>
2025-06-01 23:03:48 -0400

    Version # And removed sample info

--- src/main/java/com/jcrail/jcrec/JCRec.java
+++ src/main/java/com/jcrail/jcrec/JCRec.java
@@ -16,7 +16,7 @@ public class JCRec
 {
     public static final String MODID = "jcrec";
     public static final String NAME = "JCRecords";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.1.0";
 
     public static CreativeTabs jcrecCreativeOld;
     public static CreativeTabs jcrecCreative70s;
