84a5502a8fa95848b86c328a3ca7395c3d6139c0
JCRail <terrillbrayden@gmail>
2025-07-17 13:39:26 -0700

    The great thinning

--- src/main/java/com/jcrail/jcrec/JCRec.java
+++ src/main/java/com/jcrail/jcrec/JCRec.java
@@ -17,46 +17,17 @@ public class JCRec
     public static final String MODID = "jcrec";
     public static final String NAME = "JCRecords";
     public static final String VERSION = "1.1.0";
-
-    public static CreativeTabs jcrecCreativeOld;
-    public static CreativeTabs jcrecCreative70s;
-    public static CreativeTabs jcrecCreative80s;
-    public static CreativeTabs jcrecCreative90s;
-    public static CreativeTabs jcrecCreativeModern;
+    
     public static CreativeTabs jcrecCreativeGames;
     public static CreativeTabs jcrecCreativeRailroad;
     public static CreativeTabs jcrecCreativeTracks;
+
     public static Logger jcrecLogger = LogManager.getLogger("JCRecords");
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
         //Initalize Creative Tabs.
         jcrecLogger.info("JCRecords is flipping the tracks");
-
-        jcrecCreativeOld = new CreativeTabs("JCRecords - The Old Tracks") {
-            public Item getTabIconItem() {
-                return ItemIDs.disc_highwayhell.item; }
-            public String getTranslatedTabLabel() { return "JCRecords - The Old Tracks"; }};
-
-        jcrecCreative70s = new CreativeTabs("JCRecords - The 70s Tracks") {
-            public Item getTabIconItem() {
-                return ItemIDs.disc_ballblitz.item; }
-            public String getTranslatedTabLabel() { return "JCRecords - The 70s Tracks"; }};
-
-        jcrecCreative80s = new CreativeTabs("JCRecords - The 80s Tracks") {
-            public Item getTabIconItem() {
-                return ItemIDs.disc_barra.item; }
-            public String getTranslatedTabLabel() { return "JCRecords - The 80s Tracks"; }};
-
-        jcrecCreative90s = new CreativeTabs("JCRecords - The 90s Tracks") {
-            public Item getTabIconItem() {
-                return ItemIDs.disc_blue.item; }
-            public String getTranslatedTabLabel() { return "JCRecords - The 90s Tracks"; }};
-
-        jcrecCreativeModern = new CreativeTabs("JCRecords - The Modern Tracks") {
-            public Item getTabIconItem() {
-                return ItemIDs.disc_fallingtodo.item; }
-            public String getTranslatedTabLabel() { return "JCRecords - The Modern Tracks"; }};
 
         jcrecCreativeGames = new CreativeTabs("JCRecords - The Game Tracks") {
             public Item getTabIconItem() {
